c5437d485672250ca7256024710eae895b5ef143
coverse123 <wcoverseapp@gmail.com>
2025-11-03 04:49:41 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Tu archivo firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSy...",
+  authDomain: "tu-proyecto.firebaseapp.com",
+  projectId: "tu-proyecto",
+  storageBucket: "tu-proyecto.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef123456"
+};
